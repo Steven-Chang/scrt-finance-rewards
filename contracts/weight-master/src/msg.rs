67cf89e0094057ae9bbdedc4ced4a708bc0691f7
TomL94 <langer.tom7@gmail.com>
2021-02-28 16:15:27 +0200

    Messages and state

--- contracts/weight-master/src/msg.rs
+++ contracts/weight-master/src/msg.rs
@@ -1,27 +1,33 @@
+use cosmwasm_std::{HumanAddr, Uint128};
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct InitMsg {
-    pub count: i32,
+    pub gov_token_addr: HumanAddr,
+    pub gov_token_hash: String,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum HandleMsg {
-    Increment {},
-    Reset { count: i32 },
+    // Callbacks
+    MintFor { address: HumanAddr, amount: Uint128 },
+
+    // Admin commands
+    SetWeights {},
+    SetGovToken { addr: HumanAddr, hash: String },
+    ChangeAdmin { addr: HumanAddr },
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum QueryMsg {
-    // GetCount returns the current count as a json-encoded number
-    GetCount {},
+    GetAllocation {},
 }
 
-// We define a custom struct for each query response
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub struct CountResponse {
-    pub count: i32,
-}
+// // We define a custom struct for each query response
+// #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+// pub struct CountResponse {
+//     pub count: i32,
+// }
--- contracts/weight-master/src/state.rs
+++ contracts/weight-master/src/state.rs
@@ -1,15 +1,17 @@
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
-use cosmwasm_std::{CanonicalAddr, Storage};
+use cosmwasm_std::{HumanAddr, Storage};
 use cosmwasm_storage::{singleton, singleton_read, ReadonlySingleton, Singleton};
 
 pub static CONFIG_KEY: &[u8] = b"config";
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct State {
-    pub count: i32,
-    pub owner: CanonicalAddr,
+    pub admin: HumanAddr,
+    pub gov_token_addr: HumanAddr,
+    pub gov_token_hash: String,
+    pub total_alloc_points: u128,
 }
 
 pub fn config<S: Storage>(storage: &mut S) -> Singleton<S, State> {
@@ -18,4 +20,11 @@ pub fn config<S: Storage>(storage: &mut S) -> Singleton<S, State> {
 
 pub fn config_read<S: Storage>(storage: &S) -> ReadonlySingleton<S, State> {
     singleton_read(storage, CONFIG_KEY)
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct RewardContract {
+    pub alloc_points: u128,
+    pub last_update_block: u64,
+    pub eligible_for: u128,
 }
